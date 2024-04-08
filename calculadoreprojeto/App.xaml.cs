ec9d9d2429ec2ae9d59d7e68dc087b1d65dac33c
Usuario <Usuario@CPD03-M02>
2024-04-08 14:16:34 -0300

    Adicionar arquivos de projeto.

---
+++ calculadoreprojeto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace calculadoreprojeto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ calculadoreprojeto/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace calculadoreprojeto
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
